10c249272a15193fd99c90c4e1b1481f9917dc48
Chad Chapman <chad.chapman2010@gmail.com>
2026-02-03 21:00:56 -0500

    Removed border, added shadow, and fixed styling

--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -6,9 +6,9 @@
 <?php $this->start('body'); ?>
 
 <div class="container">
-  <div class="welcome w-75 mx-auto bg-white">
-    <h1 class="ms-3 my-3"><strong>Welcome to Chirper!</strong></h1>
-    <p class="ms-3 my-3">This is your brand new Chappy.php application.  Time to make it sing (or chirp)!</p>
+  <div class="welcome w-75 mx-auto bg-white shadow-lg">
+    <h1 class="ms-3 my-3 pt-3"><strong>Welcome to Chirper!</strong></h1>
+    <p class="ms-3 my-3 pb-3">This is your brand new Chappy.php application.  Time to make it sing (or chirp)!</p>
   </div>
 </div>
 
