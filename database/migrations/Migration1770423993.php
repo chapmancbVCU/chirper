f776192c36a909ac0d8aa3eeacfdcae6fae74695
Chad Chapman <chad.chapman2010@gmail.com>
2026-02-06 19:58:08 -0500

    Updated migration

--- database/migrations/Migration1770423993.php
+++ database/migrations/Migration1770423993.php
@@ -1,5 +1,7 @@
 <?php
 namespace Database\Migrations;
+
+use Core\DB;
 use Core\Lib\Database\Schema;
 use Core\Lib\Database\Blueprint;
 use Core\Lib\Database\Migration;
@@ -16,8 +18,21 @@ class Migration1770423993 extends Migration {
     public function up(): void {
         Schema::create('chirps', function (Blueprint $table) {
             $table->id();
-
+            $table->integer('user_id');
+            $table->index('user_id');
+            $table->timestamps();
+            $table->string('message', 255);
         });
+
+        $db = DB::getInstance();
+        $fields = [
+            'id' => 1,
+            'user_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'message' => 'My first chirp'
+        ];
+        $db->insert('chirps', $fields);
     }
 
     /**
